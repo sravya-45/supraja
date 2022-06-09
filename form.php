bc5feea19f780a2252ee0be8935b3576cd6e5836
sravya-45 <106236550+sravya-45@users.noreply.github.com>
2022-06-09 14:14:08 +0530

    Create form.php

---
+++ form.php
@@ -0,0 +1,43 @@
+<html>
+    
+    <body bgcolor="grey">
+        
+        
+        <center><h1 >Register Form</h1></center><br><br>
+        <form action="function.php" method="POST">
+        <table align="center">
+            <tr>
+                <td>name</td>
+                <td>
+                    <input type="text" name="username" >
+                </td>
+            </tr>
+            <tr>
+                <td>password</td>
+                <td>
+                    <input type="password" name="password">
+                </td>
+            </tr>
+            <tr>
+                <td>email</td>
+                <td>
+                    <input type="email" name="email">
+                </td>
+            </tr>
+            <tr>
+                <td>phonecode</td>
+                <td>
+                    <input type="text" name="phonecode">
+                </td>
+            </tr>
+            <tr>
+               
+                <td>
+                    <input type="submit" value="submit">
+                </td>
+            </tr>
+        </table>
+        </form>
+    
+    </body>
+</html>
